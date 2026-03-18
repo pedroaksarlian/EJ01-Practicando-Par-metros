d397a2752f7fe89670176de0bb48d310b41faf67
PEDRO AKSARLIAN <49554455@ort>
2026-03-18 09:03:46 -0300

    Creacion de constructor y variables de clase Tarea

---
+++ Tarea.cs
@@ -0,0 +1,14 @@
+public class Tarea
+{
+    private int id;
+    private string descripcion;
+    private bool completada;
+
+    public Tarea()
+    {
+        this.id = 1;
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+}
